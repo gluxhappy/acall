265939bef4767b4e0ae8684f7bf39e45270b57d0
gluxhappy <gluxhappy@gmail.com>
2016-04-26 12:00:01 +0800

    Develop: add comment to RequestReceiver

--- src/main/java/me/glux/acall/RequestReceiver.java
+++ src/main/java/me/glux/acall/RequestReceiver.java
@@ -2,7 +2,14 @@ package me.glux.acall;
 
 import me.glux.acall.protocal.RequestEntity;
 
+/**
+ * RequestReceiver for receiving acall request from remote(local queue or mq server)
+ */
 public interface RequestReceiver {
+	/**
+	 * Register current server name as 'server'
+	 * @param server name of server
+     */
 	void registerServer(String server);
 	void registerApi(String api);
 	RequestEntity receive() throws InterruptedException;
